8cca528c240d0bb9a8be0dea759193483a030f95
Daniel Marino <starzonmyarmz@gmail.com>
2021-04-13 14:22:19 -0400

    sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const pluginSass = require("eleventy-plugin-sass")
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: 'styles/*.scss',
+    outputDir: 'dist',
+    sourcemaps: true
+  })
+
   return {
     dir: {
       input: "src",
